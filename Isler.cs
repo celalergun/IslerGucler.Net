639cb99814849b7792c23de82ca417ecc5cac3ab
Celal Ergün <celalergun@gmail.com>
2019-07-17 14:10:19 +0300

    Job (isler) and JobDetail (islerdetay) classes are added

---
+++ Isler.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IslerGucler.Net
+{
+    public class Isler
+    {
+        public int IslerID { get; set; }
+        
+        // job name
+        public string IsAdi { get; set; }
+        
+        // start time
+        public DateTime Baslama { get; set; }
+        
+        // state
+        public string Durum { get; set; }
+        
+        // is finished
+        public bool BittiMi { get; set; }
+    }
+}
---
+++ IslerDetay.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IslerGucler.Net
+{
+    public class IslerDetay
+    {
+        public int IslerDetayID { get; set; }
+
+        // master ID
+        public int IslerID { get; set; }
+
+        // start date/time
+        public DateTime Baslama { get; set; }
+
+        // finish time
+        public DateTime Bitis { get; set; }
+
+        // description
+        public string Aciklama { get; set; }
+    }
+}
